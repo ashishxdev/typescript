1a047061c70f563890b187b7e7893a4d622747e7
Aashish Rana <ashishrana2004@gmail.com>
2026-04-15 21:46:21 +0530

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,5 @@
+// function greet(name) {
+//     return "Hello " + name
+// }
+// console.log(greet("Aashish"))
+// console.log(greet(32))
---
+++ basic.ts
@@ -0,0 +1,5 @@
+function greet(name: string): string {
+  return `Hello ${name}`;
+}
+console.log(greet("Aashish"));
+console.log(greet(32)); // This is running because in js it can run properly
